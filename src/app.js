17756f0ceffdd6b4e00c4c2a5d19bafa83be660b
Thandokazi2000 <thandonotyalwa@gmail.com>
2023-09-08 12:25:31 +0200

    Updated JS

--- src/app.js
+++ src/app.js
@@ -21,16 +21,25 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes} `;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Fri", "Sat", "Sun", "Mon", "Tue"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
       <div class="col-2">
         <div class="weather-forecast-date">${day}</div>
         <img
@@ -45,6 +54,7 @@ function displayForecast(response) {
       </div>
       
   `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
